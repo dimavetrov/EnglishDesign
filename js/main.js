4f787e89212fa28cec61e1e7c2a9db5426eef186
Dima Vetrov <ddvetrov94@gmail.com>
2016-05-07 20:12:15 +0300

    Library page part 1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav:false,
+    dots:true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            loop: false,
+            margin: 20
+        },
+        600: {
+            items: 2,
+            loop: false,
+            margin: 20
+        },
+        1300: {
+            items: 3,
+            loop: false,
+            margin: 20
+        }
+
+    }
+});
--- js/script.js
+++ js/script.js
@@ -26,53 +26,27 @@ $(document).ready(function(){
     });
 
 
-
     $('.up').click(function(){
         $('body').animate({
             scrollTop: 0
         },1000);
         return false;
     });
+
 if( $(window).width >800) {
     var topBlockHeight = $('.cathedra-block').height() + $('.nav-block').height();
     var wind = $(window).height();
     $('.auth-block').height(wind - topBlockHeight - '50');
 }
-/*
+
     $('.owl-dot').on('click',function(){
         $('.owl-dot',this).toggleClass('active');
     });
 
 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav:false,
-        dots:true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                loop: false,
-                margin: 20
-            },
-            600: {
-                items: 2,
-                loop: false,
-                margin: 20
-            },
-            1300: {
-                items: 3,
-                loop: false,
-                margin: 20
-            }
 
-        }
-    });
 
-*/
 
-    $(document).ready(function() {
         $('.popup-gallery').magnificPopup({
             delegate: 'a',
             type: 'image',
@@ -90,12 +64,6 @@ if( $(window).width >800) {
                 }
             }
         });
-    });
-
-
-
-
-
 
 
 
